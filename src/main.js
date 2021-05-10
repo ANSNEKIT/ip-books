f430d116e85176e4e1ff2695fd8eac06a4831755
ansnekit <ansnekit@gmail.com>
2021-05-10 23:54:42 +0300

    init project

--- src/main.js
+++ src/main.js
@@ -1,5 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
+import '@babel/polyfill';
+import 'mutationobserver-shim';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import './app.css';
 
 createApp(App).use(router).mount('#app');
